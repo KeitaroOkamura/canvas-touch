569d310041c75a4952c3606cff15aae0a179e8c0
Keitaro Okamura <peter.okamura@karabiner.tech>
2019-04-20 23:26:13 +0900

    images add

--- src/images/templates/templatelist.js
+++ src/images/templates/templatelist.js
@@ -1,88 +1,80 @@
-// Faces
-const facelist = [
-
-	require('./faces/1.png'),
-	require('./faces/2.png'),
-	require('./faces/3.png'),
-	require('./faces/4.png'),
-	require('./faces/5.png'),
-	require('./faces/6.png'),
-
+// Animals
+const animallist = [
+  require("./animals/1.png"),
+  require("./animals/2.png"),
+  require("./animals/3.png"),
+  require("./animals/4.png"),
+  require("./animals/5.png"),
+  require("./animals/6.png"),
+  require("./animals/7.png"),
+  require("./animals/8.png"),
+  require("./animals/9.png"),
 ]
 
-
-// Eyes
-const eyeslist = [
-
-	require('./eyes/1.png'),
-	require('./eyes/2.png'),
-	require('./eyes/3.png'),
-	require('./eyes/4.png'),
-	require('./eyes/5.png'),
-	require('./eyes/6.png'),
-
+// Foods
+const foodlist = [
+  require("./foods/1.png"),
+  require("./foods/2.png"),
+  require("./foods/3.png"),
+  require("./foods/4.png"),
+  require("./foods/5.png"),
+  require("./foods/6.png"),
+  require("./foods/7.png"),
+  require("./foods/8.png"),
+  require("./foods/9.png"),
+  require("./foods/10.png"),
+  require("./foods/11.png"),
+  require("./foods/12.png"),
+  require("./foods/13.png"),
+  require("./foods/14.png"),
+  require("./foods/15.png"),
+  require("./foods/16.png"),
+  require("./foods/17.png"),
+  require("./foods/18.png"),
+  require("./foods/19.png"),
 ]
 
-
-// Backgrounds
-const bglist = [
-
-	require('./bgs/1.png'),
-	require('./bgs/2.png'),
-	require('./bgs/3.png'),
-	require('./bgs/4.png'),
-	require('./bgs/5.png'),
-	require('./bgs/6.png'),
+// Humans
+const humanlist = [
+  require("./humans/1.png"),
+  require("./humans/2.png"),
+  require("./humans/3.png"),
+  require("./humans/4.png"),
+  require("./humans/5.png"),
+  require("./humans/6.png"),
+  require("./humans/7.png"),
+  require("./humans/8.png"),
+  require("./humans/9.png"),
+  require("./humans/10.png"),
 ]
 
-
-
-
-// HairStyles
-const hairlist = [
-
-	require('./hair/1.png'),
-	require('./hair/2.png'),
-	require('./hair/3.png'),
-	require('./hair/4.png'),
-	require('./hair/5.png'),
-	require('./hair/6.png'),
-	require('./hair/7.png'),
-	require('./hair/8.png'),
-	require('./hair/9.png'),
-	require('./hair/10.png'),
-	require('./hair/11.png'),
-	require('./hair/12.png'),
-	require('./hair/13.png'),
-	require('./hair/14.png'),
-	require('./hair/15.png'),
-	require('./hair/16.png'),
-	require('./hair/17.png'),
-	require('./hair/18.png'),
-
+// Items
+const itemlist = [
+  require("./items/1.png"),
+  require("./items/2.png"),
+  require("./items/3.png"),
+  require("./items/4.png"),
+  require("./items/5.png"),
+  require("./items/6.png"),
 ]
 
-
-// Beards
-const faciallist = [
-
-	require('./facialhair/1.png'),
-	require('./facialhair/2.png'),
-	require('./facialhair/3.png'),
-	require('./facialhair/4.png'),
-	require('./facialhair/5.png'),
-	require('./facialhair/6.png'),
-	require('./facialhair/7.png'),
-	require('./facialhair/8.png'),
-	require('./facialhair/9.png'),
-
+// Others
+const otherlist = [
+  require("./others/1.png"),
+  require("./others/2.png"),
+  require("./others/3.png"),
+  require("./others/4.png"),
+  require("./others/5.png"),
+  require("./others/6.png"),
+  require("./others/7.png"),
+  require("./others/8.png"),
+  require("./others/9.png"),
+  require("./others/10.png"),
+  require("./others/11.png"),
+  require("./others/12.png"),
+  require("./others/13.png"),
+  require("./others/14.png"),
+  require("./others/15.png"),
 ]
 
-
-export {
-	hairlist, 
-	bglist, 
-	faciallist,
-	facelist,
-	eyeslist
-};
+export { animallist, foodlist, humanlist, itemlist, otherlist }
