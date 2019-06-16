85f1cb21e8c995223b44aab1bf7598753c64ff12
Keitaro Okamura <peter.okamura@karabiner.tech>
2019-06-16 23:15:50 +0900

    テンプレート画像修正

--- src/images/templates/templatelist.js
+++ src/images/templates/templatelist.js
@@ -1,80 +1,24 @@
-// Animals
-const animallist = [
-  require("./animals/1.png"),
-  require("./animals/2.png"),
-  require("./animals/3.png"),
-  require("./animals/4.png"),
-  require("./animals/5.png"),
-  require("./animals/6.png"),
-  require("./animals/7.png"),
-  require("./animals/8.png"),
-  require("./animals/9.png"),
+// Marks
+const marklist = [
+  { name: "マーク1", path: require("./marks/1.png") },
+  { name: "マーク2", path: require("./marks/2.png") },
+  { name: "マーク3", path: require("./marks/3.png") },
+  { name: "マーク4", path: require("./marks/4.png") },
+  { name: "マーク5", path: require("./marks/5.png") },
+  { name: "マーク6", path: require("./marks/6.png") },
+  { name: "マーク7", path: require("./marks/7.png") },
+  { name: "マーク8", path: require("./marks/8.png") },
+  { name: "マーク9", path: require("./marks/9.png") }
 ]
 
-// Foods
-const foodlist = [
-  require("./foods/1.png"),
-  require("./foods/2.png"),
-  require("./foods/3.png"),
-  require("./foods/4.png"),
-  require("./foods/5.png"),
-  require("./foods/6.png"),
-  require("./foods/7.png"),
-  require("./foods/8.png"),
-  require("./foods/9.png"),
-  require("./foods/10.png"),
-  require("./foods/11.png"),
-  require("./foods/12.png"),
-  require("./foods/13.png"),
-  require("./foods/14.png"),
-  require("./foods/15.png"),
-  require("./foods/16.png"),
-  require("./foods/17.png"),
-  require("./foods/18.png"),
-  require("./foods/19.png"),
+// Bases
+const baselist = [
+  { name: "ベース1", path: require("./bases/1.png") },
+  { name: "ベース2", path: require("./bases/2.png") },
+  { name: "ベース3", path: require("./bases/3.png") },
+  { name: "ベース4", path: require("./bases/4.png") },
+  { name: "ベース5", path: require("./bases/5.png") },
+  { name: "ベース6", path: require("./bases/6.png") }
 ]
 
-// Humans
-const humanlist = [
-  require("./humans/1.png"),
-  require("./humans/2.png"),
-  require("./humans/3.png"),
-  require("./humans/4.png"),
-  require("./humans/5.png"),
-  require("./humans/6.png"),
-  require("./humans/7.png"),
-  require("./humans/8.png"),
-  require("./humans/9.png"),
-  require("./humans/10.png"),
-]
-
-// Items
-const itemlist = [
-  require("./items/1.png"),
-  require("./items/2.png"),
-  require("./items/3.png"),
-  require("./items/4.png"),
-  require("./items/5.png"),
-  require("./items/6.png"),
-]
-
-// Others
-const otherlist = [
-  require("./others/1.png"),
-  require("./others/2.png"),
-  require("./others/3.png"),
-  require("./others/4.png"),
-  require("./others/5.png"),
-  require("./others/6.png"),
-  require("./others/7.png"),
-  require("./others/8.png"),
-  require("./others/9.png"),
-  require("./others/10.png"),
-  require("./others/11.png"),
-  require("./others/12.png"),
-  require("./others/13.png"),
-  require("./others/14.png"),
-  require("./others/15.png"),
-]
-
-export { animallist, foodlist, humanlist, itemlist, otherlist }
+export { marklist, baselist }
